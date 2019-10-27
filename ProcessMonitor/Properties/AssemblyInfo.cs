89b5b295d14200440e6ddc5fc44735b77978a93f
SeppPenner <HansM2013@gmx.de>
2019-10-27 20:53:08 +0100

    Updated nuget packages, added GitVersionTask.

--- ProcessMonitor/Properties/AssemblyInfo.cs
+++ ProcessMonitor/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("6460971a-464f-4e73-8803-f08701e1a4cf")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
--- README.md
+++ README.md
@@ -21,6 +21,7 @@ The executable is written and tested in .Net 4.8.
 Change history
 --------------
 
+* **Version 1.0.1.0 (2019-10-27)** : Updated nuget packages, added GitVersionTask.
 * **Version 1.0.0.3 (2019-05-06)** : Updated .Net version to 4.8.
 * **Version 1.0.0.2 (2017-03-24)** : Updated Languages.dll to version 1.0.0.4.
 * **Version 1.0.0.1 (2017-03-21)** : Updated Languages.dll.
